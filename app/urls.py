542c6e0608e4e4c48075bb105d5125d2dc653aa2
ShaunSEMO <tshegofatsosemenya@gmail.com>
2021-01-11 11:47:10 +0200

    Front end

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.redir, name='redir'),
+    path('', TemplateView.as_view(template_name='index.html'), name='redir'),
     path('platforms', views.platforms, name='platforms'),
     path('platforms/<int:id>', views.routes, name='routes'),
 ]
