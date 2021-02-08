4af226a6b1c4acf00ac2814991e393b3ffbad6ed
ShaunSEMO <tshegofatsosemenya@gmail.com>
2021-02-08 17:29:49 +0200

    Refresher

--- fika_1/settings.py
+++ fika_1/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'fika_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'fika_front/build'), 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'ionicFikaFrontend/src'), 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -156,8 +156,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'ionicFrontend/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'ionicFrontend/assets')
 ]
