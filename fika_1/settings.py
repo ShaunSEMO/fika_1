ebde364da273ceda62f4631e2f090ba8b2b59759
ShaunSEMO <tshegofatsosemenya@gmail.com>
2021-03-26 11:58:24 +0200

    Static files fix

--- fika_1/settings.py
+++ fika_1/settings.py
@@ -164,10 +164,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'ionicFrontend/assets/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'ionicFrontend/assets')
+    os.path.join(BASE_DIR, 'static/')
 ]
 
 MEDIA_URL = '/media/'
